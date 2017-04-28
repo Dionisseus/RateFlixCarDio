30fe38106260cfbe3d342b6e88dfbb6e91905f04
Dionisseus <dionisseus666@gmail.com>
2017-04-28 11:30:28 -0500

    Validaciones Profile
    faltaban

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -32,21 +32,21 @@
             <span id="mainInfoContainer">
              <div id="profileImg" style="background-image:url({{Auth::user()->avatar}})"></div>
                 <h1 id="profileName">{{Auth::user()->name}}</h1>
-                <input name="name"type="text" value="{{Auth::user()->name}}">
+                <input name="name"type="text" value="{{Auth::user()->name}}" required>
                 <div id="update"></div>
             </span>
             <h2>Email</h2>
             <span class="data">{{Auth::user()->email}}</span>
-            <input type="text" name="email" value="{{Auth::user()->email}}">
+            <input type="text" name="email" value="{{Auth::user()->email}}" required>
             <h2>Fecha de nacimiento</h2>
             <span class="data">06 de Octubre de 1994</span>
-            <input type="date" name="birth-date" value="">
+            <input type="date" name="birth-date" value="" required>
             <h2>Genero</h2>
             <span class="data">{{Auth::user()->genre}}</span>
-            <input type="text"name="genre" value="{{Auth::user()->genre}}">
+            <input type="text"name="genre" value="{{Auth::user()->genre}}" required>
             <h2 class="img">Imagen de perfil</h2>
             <label for="profilePic">Selecciona una imagen</label>
-            <input id="profilePic" name="avatar" class="hidden" type="file">
+            <input id="profilePic" name="avatar" class="hidden" type="file" required>
             <input type="submit" value="Guardar perfil">
 
         </form>
